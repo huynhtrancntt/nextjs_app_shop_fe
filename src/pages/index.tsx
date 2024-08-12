bc65e81c8cf19ada3bb8c69c583b81448b56cfbe
huynhtrancntt <huynhtrancntt@gmail.com>
2024-08-12 15:07:46 +0700

    update UI login/register

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,18 +3,20 @@ import Head from 'next/head'
 import Button from '@mui/material/Button'
 import axios from 'axios'
 import { useEffect } from 'react'
+import CustomTextField from 'src/components/text-field'
+import { Box } from '@mui/material'
 
 export default function Home() {
-  const fetchApiUser = async () => {
-    await axios.get("http://localhost:8000/api/users?limit=10&page=1&order=created%20asc").then((res) => {
-      console.log("res", { res });
+  // const fetchApiUser = async () => {
+  //   await axios.get("http://localhost:8000/api/users?limit=10&page=1&order=created%20asc").then((res) => {
+  //     console.log("res", { res });
 
-    })
-  }
+  //   })
+  // }
 
-  useEffect(() => {
-    fetchApiUser()
-  }, [])
+  // useEffect(() => {
+  //   fetchApiUser()
+  // }, [])
 
   return (
     <>
@@ -24,7 +26,10 @@ export default function Home() {
         <meta name='viewport' content='width=device-width, initial-scale=1' />
         <link rel='icon' href='/favicon.ico' />
       </Head>
-      <Button variant='contained'>Hello world HT  01 </Button>
+      <Box sx={{ margin: 10, width: '200px' }}>
+        <CustomTextField label='test' />
+      </Box>
+
     </>
   )
 }
